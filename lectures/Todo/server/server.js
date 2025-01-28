09d034c2c111f7dc10779866efa43a1bc274ee39
unknown <veneyamanda1@gmail.com>
2025-01-27 20:06:46 -0500

    todo

--- lectures/Todo/server/server.js
+++ lectures/Todo/server/server.js
@@ -15,7 +15,7 @@ const Schema = mongoose.Schema
 const ToDoSchema = new Schema(
     {
         todo: String,
-        created: Date
+        created: Number
     }
 )
 const ToDo = mongoose.model('ToDo', ToDoSchema)
@@ -52,5 +52,6 @@ app.listen(PORT, () => {
         .then(() => {
             console.log("Connected to Database")
         })
+        .catch(err => console.log(err))
     console.log(`Server is runnning on port ${PORT}`)
 })
